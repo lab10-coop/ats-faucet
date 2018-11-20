a827581213648dea4c27c7cfc63508e67924b744
didi <git@d10r.net>
2018-11-20 22:31:53 +0100

    fixed indentation

--- index.js
+++ index.js
@@ -8,32 +8,32 @@ var web3 = null;
 
 // wrap the init stuff into async main in order to have await available
 async function start() {
-	if(isNaN(config.amount)) {
-		console.error('ERR: no valid amount configured in "faucet.amount"');
-		process.exit(1);
-	}
+  if(isNaN(config.amount)) {
+    console.error('ERR: no valid amount configured in "faucet.amount"');
+    process.exit(1);
+  }
 
-	web3 = new Web3(new Web3.providers.HttpProvider(config.network.rpc));
+  web3 = new Web3(new Web3.providers.HttpProvider(config.network.rpc));
 
-	http.createServer(handleRequest).listen(config.network.port, config.network.interface);
-	console.log(`http server listening at interface ${config.network.interface} port ${config.network.port}`);
+  http.createServer(handleRequest).listen(config.network.port, config.network.interface);
+  console.log(`http server listening at interface ${config.network.interface} port ${config.network.port}`);
 }
 
 function handleRequest(req, res) {
-	var pathname = url.parse(req.url).pathname;
-	console.log(`request for ${pathname}`);
+  var pathname = url.parse(req.url).pathname;
+  console.log(`request for ${pathname}`);
 
-	res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', '*');
 
-	// check if it's a path we care about
-	var splitPath = url.parse(req.url).path.split('/');
-	if(splitPath[1].startsWith('0x')) {
-		var userAddr = splitPath[1];
-		if(! web3.utils.isAddress(userAddr)) {
-			res.writeHead(401, {'Content-Type': 'text/plain'});
-			res.end(`not a valid address: ${userAddr}\n`);
-			return;
-		}
+  // check if it's a path we care about
+  var splitPath = url.parse(req.url).path.split('/');
+  if(splitPath[1].startsWith('0x')) {
+    var userAddr = splitPath[1];
+    if(! web3.utils.isAddress(userAddr)) {
+      res.writeHead(401, {'Content-Type': 'text/plain'});
+      res.end(`not a valid address: ${userAddr}\n`);
+      return;
+    }
 
     console.log(`processing for ${userAddr}`);
     refuelAccount(userAddr, (err, txHash) => {
@@ -57,15 +57,15 @@ function handleRequest(req, res) {
 
 // sends some coins to the given account <userAddr>, invokes the given callback with the resulting transaction hash
 async function refuelAccount(userAddr, callback) {
-	console.log(`sending ${config.amount} ATS to ${userAddr}...`);
+  console.log(`sending ${config.amount} ATS to ${userAddr}...`);
 
-	const txObj = {
-		from: config.account.address,
-		to: userAddr,
-		value: web3.utils.toWei(config.amount.toString()),
-		gas: config.gas
-	};
-	const signedTxObj = await web3.eth.accounts.signTransaction(txObj, config.account.privateKey);
+  const txObj = {
+    from: config.account.address,
+    to: userAddr,
+    value: web3.utils.toWei(config.amount.toString()),
+    gas: config.gas
+  };
+  const signedTxObj = await web3.eth.accounts.signTransaction(txObj, config.account.privateKey);
 
   web3.eth.sendSignedTransaction(signedTxObj.rawTransaction)
     .once('transactionHash', function (txHash) {
